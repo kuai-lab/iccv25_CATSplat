2ce7d19d532e95d122f2f19cf31d0c4dcd8f2c04
Wonseok Roh <wonseok96@gmail.com>
2025-07-02 16:03:26 +0900

    Remove files

--- data_download.sh
+++
@@ -1,2 +0,0 @@
-python datasets/download_realestate10k.py -d data/RealEstate10K -o data/RealEstate10K -m train
-python datasets/download_realestate10k.py -d data/RealEstate10K -o data/RealEstate10K -m test
